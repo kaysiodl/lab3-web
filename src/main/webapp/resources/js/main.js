3d52030c086c4af220bccf2f003c856c5555411c
kaysiodl <polinamatveeva3010@gmail.com>
2025-11-13 23:45:46 +0300

    without db

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -84,16 +84,6 @@ function clearErrors() {
     document.querySelectorAll('.error-text').forEach(element => element.remove());
 }
 
-clearButton.addEventListener('click', () => {
-    document.querySelectorAll('.table-row').forEach(element => element.remove());
-    clearSession().then(() => {
-        draw();
-        drawLabels();
-        resultsFromBean.clear();
-        localStorage.setItem('selectedR', 'R');
-    });
-});
-
 async function clearSession() {
     try {
         await fetch('/lab2/clear', {
